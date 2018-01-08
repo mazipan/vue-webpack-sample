da135a017f911dee25cbef588d2845b7f2c4b18a
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-08 18:23:56 +0700

    :rocket: Initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    Hello, I am About page !
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.about{
+
+}
+</style>
