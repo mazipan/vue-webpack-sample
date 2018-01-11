78e393c1276854e572ad73e8982ebe727a21ecc9
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-11 16:08:34 +0700

    add proxy table

--- config/index.js
+++ config/index.js
@@ -10,11 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/api/": {
+        target: "http://localhost:8080/",
+        changeOrigin: true
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
